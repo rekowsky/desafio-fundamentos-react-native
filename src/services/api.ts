9d4c22cdc835270e87495564300134a6f6973c1a
Ricardo Rekowsky <ricardo@perspectivanet.com.br>
2021-03-02 12:06:29 -0300

    challenge concluded

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
-
+// http://localhost:3333
+// http://10.0.2.2:3000 para Android simulator
 const api = axios.create({
-  baseURL: 'http://localhost:3333',
+  baseURL: 'http://10.0.2.2:3333',
 });
 
 export default api;
